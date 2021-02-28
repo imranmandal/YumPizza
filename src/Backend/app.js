c4c3ad6d498bc9e0fe09cfadfbf84cb1d35eab85
imranmandal <64127903+imranmandal@users.noreply.github.com>
2021-02-28 15:34:56 +0530

    registration added

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -13,11 +13,14 @@ const cors = require('cors');
 const app = express(); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))  ;
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
 
 app.use(session({
-        secret: 'secretcode',
-        resave: true,
+        secret: 'secret-code',
+        resave: false,
         saveUninitialized: false
 }));
 app.use(passport.initialize());
@@ -37,8 +40,18 @@ connection.once('open', ()=>{
 
 
 const userSchema = new mongoose.Schema ({
-    username: String,
-    pass: String,
+    email: {
+        type: String,
+        unique: true,
+        dropDups: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        dropDups: true
+    },
+    password: String,
+    userType: String
 });
 
 // -----------------------------------------------------
@@ -96,7 +109,6 @@ app.route("/items")
         const {img,name,category,timed,from,to,status} = req.body;
         const oneItem = new Item({
             img: {
-                // data: fs.readFileSync(path.join(__dirname + '/uploads/' + img)),
                 data: img,
                 contentType: 'image/png'
             },
@@ -155,18 +167,22 @@ app.get("/register",(req, res)=>{
 
 app.post("/register",(req, res)=>{ 
 
-    User.register({ username: req.body.username }, req.body.password, (err, user)=>{
+    User.register({ username: req.body.username, email:req.body.email, userType: req.body.userType }, req.body.password, (err, user)=>{
     err ?
-        console.log(err)
+        res.send(err.message)
     :
-        passport.authenticate('local')(req, res, ()=>(res.send(req.user)))
+        passport.authenticate('local')(req, res, ()=>{(res.send("User Created."))})
     });
-    // res.send(req.body);
 });
 
 
-app.get("/user",(req, res)=>{
-    res.send(req.user);
+app.get("/user", (req, res)=>{
+    const {_id, email, username} = req.user;
+    res.send({
+        id: _id,
+        email: email,
+        name: username,
+    });
 });
 
 
@@ -177,209 +193,3 @@ app.listen(4000, function(){
     console.log("server started on port 4000.")
 })
 
-
-//jshint esversion:6
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const session = require("express-session");
-// const passport = require("passport");
-// const passportLocalMongoose = require("passport-local-mongoose");
-
-
- 
-// const app = express(); 
-// app.use(express.json());
-// app.use(cors());
-
-
-// app.use(session({
-//         secret: "code is wrong",
-//         resave: true,
-//         saveUninitialized: true,
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
-
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-// const connection = mongoose.connection;
-// connection.once("open", ()=>{
-//     console.log("connetion established.");
-// });
-
-
-
-// const userSchema = new mongoose.Schema ({
-//     username: String,
-//     pass: String,
-//     type: String
-// });
-// // -----------------------------------------------------
-// userSchema.plugin(passportLocalMongoose);       //======> Adding plugin to schema...
-// // -----------------------------------------------------
-// const User = new mongoose.model("User", userSchema);
-
-// passport.use(User.createStrategy());            //===========>
-//                                                                     //copying from passport.js doc
-// // passport.serializeUser(User.serializeUser());   //===========>
-// // passport.deserializeUser(User.deserializeUser());      //====>
-// passport.serializeUser(User.serializeUser());
-  
-//   passport.deserializeUser(User.deserializeUser());
-
-
-
-// app.route("/register")
-//     .get((req, res)=>{
-//         console.log("get route log");
-//     })
-    
-//     .post((req, res)=>{        
-//         User.register({username: req.body.username, type: req.body.type}, req.body.pass, (err, user)=>{
-//             if(err){
-//                 res.send(err.message);
-//             } else {
-//                 passport.authenticate("local")(req, res, function(){
-//                     res.send("user registered.");
-//                 });
-//             } 
-                        
-//         });
-//     });
-
-
-// app.route("/login")
-//     .get((req, res)=>{
-//         console.log("get login")
-//     })
-
-//     .post((req, res)=>{
-//         const user = new User({
-//             username: req.body.username,
-//             pass: req.body.pass
-//         });
-
-//         req.login(user, (err)=>{
-//             if(err){
-//                 console.log(err.message);
-//             } else {
-//                 passport.authenticate("local")(req, res, function(){
-//                     console.log("Logged in");
-//                 });
-//             }
-//         });
-//     });
-
-// app.route("/user")
-//     .get((req, res) => {
-//         res.send(req.user);
-//     });
-
-
-
-
-
-
-// const itemSchema = new mongoose.Schema ({
-//     img: {
-//         data: Buffer,
-//         contentType: String
-//     },
-//     name: String,
-//     category: String,
-//     timed: Boolean,
-//     from: String,
-//     to: String,
-//     status: String
-// })
-
-// const Item = new mongoose.model("Item", itemSchema);
-
-
-
-
-// app.get("/",function(req, res){
-//     console.log("Welocome to home page");
-// })
-
-
-// // ------------------------------- Items route
-
-// app.route("/items") 
-
-//     .get((req, res)=>{
-//         Item.find({}, (err, data)=>{
-//             err ?
-//             console.log(err)
-//             :
-//             res.send(data);
-//     })})
-
-//     .post((req, res)=>{
-//         const {img,name,category,timed,from,to,status} = req.body;
-//         const oneItem = new Item({
-//             img: {
-//                 data: img,
-//                 contentType: "image/png"
-//             },
-//             name: name,
-//             category: category,
-//             timed: timed,
-//             from: from,
-//             to: to,
-//             status: status
-//         });
-        
-//         oneItem.save((err)=> 
-//             err ? 
-//             console.log(err) 
-//             : 
-//             console.log("inserted successfully"));
-//     })
-
-//     .put((req, res)=>{
-//         const {id, name, category, timed, status} = req.body;
-        
-//         Item.findOneAndUpdate({_id: id }, {
-//             name: name,
-//             category: category,
-//             timed: timed,
-//             status: status
-//         }, (err)=>{
-//             !err ?            
-//             console.log("Successfully Updated "+ name)
-//             :
-//             console.log(err);
-//         }).then((doc)=>{
-//             res.send(doc);
-//         })
-//     })   
-
-//     .delete((req, res)=>{
-//         const {id} = req.body;
-//         Item.findByIdAndRemove( {_id: id} ).then((doc)=>{
-//             console.log(doc);
-//             res.send(doc)
-//         })
-        
-//     });
-              
-       
-// // ----------------------------------- For Customer 
-
-// // 
-    
-
-
-
-
-
-// app.listen(4000, function(){
-//     console.log("server started on port 4000.")
-// })
--- src/Components/usersComponents/Authentication/Register.js
+++ src/Components/usersComponents/Authentication/Register.js
@@ -1,31 +1,29 @@
-import axios from 'axios';
+import axio from 'axios';
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 function Register(props){
 
     const [user, setUser] = useState({
+        email: "",
         username: "",
         password: "",
         userType: "customer",
     });
 
-    const [data, setData] = useState(null);
-    // function setUserType(userType){
-    //     props.setUser(userType)
-    // }
+    const axios = axio.create({
+        withCredentials: true
+    })
 
     const registerUser = () => {
-        // console.log(user);
         axios.post(`http://localhost:4000/register`, user)
         .then((res) => (console.log(res.data)));
-        
-        
     };
 
     const getUser = () => {
         axios.get(`http://localhost:4000/user`)
-        .then(res => (console.log(res)));
+        .then((res) => {console.log(res.data)})
+        .catch((error) => {console.log(error)})
     };
 
     const handleChange = (e) =>{
@@ -44,42 +42,22 @@ function Register(props){
         <div className="form">
             <h1>Register</h1>
             <form className="row g-3">
-                {/* <div class="col-md-6">
-                    <input type="text" class="form-control" placeholder="First Name" />
+                <div className="col-md-12">
+                    <input type="email" name="email" className="form-control" 
+                     onChange={handleChange} placeholder="Email" value={user.email} />
                 </div>
-                <div class="col-md-6">
-                    <input type="text" class="form-control" placeholder="Last Name" />
-                </div> */}
-                <div className="col-md-6">
-                    <input type="email" name="username" className="form-control" 
-                     onChange={handleChange} placeholder="Email" value={user.username} />
+                <div className="col-md-12">
+                    <input type="text" name="username" className="form-control" 
+                     onChange={handleChange} placeholder="Username" value={user.username} />
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-12">
                     <input type="password" name="password" className="form-control" 
                      onChange={handleChange} placeholder="Password" value={user.pass} />
                 </div>
-                {/* <div class="col-12">
-                    <input type="text" class="form-control" placeholder="1234 Main St" />
-                </div>
-                <div class="col-12">
-                    <input type="text" class="form-control" placeholder="Apartment, studio, or floor" />
-                </div>
-                <div class="col-md-6">
-                    <input type="text" class="form-control" placeholder="City" />
-                </div>
-                <div class="col-md-4">
-                    <input type="text" class="form-control" placeholder="State" />
-                    
-                </div>
-                <div class="col-md-2">
-                    <input type="text" class="form-control" placeholder="Zip" />
-                </div> */}
-                
-                
             </form>
             <button onClick={registerUser} className='orderBtn'>Register</button><br/>
-            <span>Already have an account? <Link to="/login" >Login</Link></span>
-            <button onClick={getUser} className='orderBtn'>Get User</button><br/>
+            <span className="col-md-12">Already have an account? <Link to="/login" >Login</Link></span>
+            <button onClick={getUser} className='orderBtn col-md-12'>Get User</button><br/>
         </div>
     </div>  
      
